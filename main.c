66fceb6893f0358eb03b0a8c7655966fc48c4623
Ian Snyder <isnide23@gmail.com>
2021-06-05 09:10:24 -0700

    add more functions and attempted recusrion for N answer

--- main.c
+++ main.c
@@ -3,27 +3,37 @@
 #include <stdio.h>
 #include <string.h>
 
+char name[100];
+char answer[100];
+
 void printGreeting() {
     printf("Welcome to the Solar System!\n");
     printf("There are eight planets and one dwarf planet to explore.\n");
-}
-
-int main(int argc, char *argv[]) {
-    char name[100];
-    char answer[100];
-
-    printGreeting();
-    // printf("Welcome to the Solar System!\n");
-    // printf("There are eight planets and one dwarf planet to explore.\n");
     printf("What is your name?\n");
     scanf("%s", name );
     printf( "Hello %s, lets explore!\n", name);
+}
+
+void askQuestion() {
     printf("Would you like to visit a random planet? (Y or N)\n");
     scanf("%s", answer);
+}
+
+
+int main(int argc, char *argv[]) {
+
+
+    printGreeting();
+    askQuestion();
+
+    
     if (strcmp(answer, "N") == 0) {
         printf("Name a planet to visit.\n");
-    } else {
+        askQuestion();
+    } else if(strcmp(answer, "Y") == 0) {
         printf("Saturn is a dope planet with rings.\n");
+    } else {
+        printf("Invalid response.\n");
     }
     return 0;
 }
